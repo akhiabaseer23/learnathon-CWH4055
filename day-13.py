6256c6974e0a29e0d5aede991c2e203eab52d590
Akhia Baseer <165878223+akhiabaseer23@users.noreply.github.com>
2025-01-01 03:15:04 +0530

    Add files via upload

---
+++ day-13.py
@@ -0,0 +1,31 @@
+#INTRODUCTION TO OOP's
+
+'''Object-oriented programming (OOP) in Python lets you structure your code by grouping related properties
+and behaviors into individual objects. You create classes as blueprints and instantiate them to form objects.
+With OOP, you can model real-world entities and their interactions, and create complex systems with reusable components. '''
+
+
+#What are the main pillars of Object-Oriented Programming (OOP)?
+'''
+ 1. **Encapsulation:** Encapsulation refers to the bundling of data (attributes) and methods (functions) that operate on the data
+ within a class. It hides the internal state of an object from the outside world, allowing access only through well-defined interfaces.
+   Encapsulation ensures data integrity, promotes modularity, and enhances code maintainability.
+
+2. **Inheritance:** Inheritance allows a new class (subclass or derived class) to inherit properties and behaviors
+      (attributes and methods) from an existing class (superclass or base class). This promotes code reuse and
+        establishes a hierarchical relationship among classes. Subclasses can extend the functionality of their superclass while
+          inheriting its common characteristics, facilitating modular design and reducing redundancy in code.
+
+3. **Polymorphism:** Polymorphism, meaning "many forms," enables objects of different classes to be treated as objects of a common
+      superclass. It allows a single interface to represent different underlying forms, promoting code flexibility and extensibility.
+        Polymorphism is achieved through method overriding, where subclasses provide their own implementation of a method defined
+          in their superclass, and method overloading, where multiple methods with the same name but different parameters can exist 
+          within a class. Polymorphism enhances code readability, maintainability, and scalability by promoting abstraction and
+            reducing dependency on specific implementations. '''
+
+
+#ABSTRACTION in Object-Oriented Programming?
+ 
+''' Abstraction in OOP allows developers to focus on essential aspects of an object while hiding unnecessary details.
+ It simplifies complex systems by defining a clear interface for interaction, promoting modularity and code reusability. 
+ Through abstraction, programmers can create efficient and scalable solutions without being burdened by implementation complexities.'''
